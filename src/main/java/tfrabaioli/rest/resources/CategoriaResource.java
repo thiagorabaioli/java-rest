0b235f4935f4b7967bfe9c39725c87c76d0bb68b
thiagorabaioli <tfrabaioli@outlook.com>
2023-05-30 16:25:55 +0100

    Domain class ok

--- src/main/java/tfrabaioli/rest/resources/CategoriaResource.java
+++ src/main/java/tfrabaioli/rest/resources/CategoriaResource.java
@@ -1,8 +1,14 @@
 package tfrabaioli.rest.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import tfrabaioli.rest.domain.Categoria;
 
 @RestController
 @RequestMapping (value = "/categorias")
@@ -10,8 +16,12 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String findAll() {
-		return "rest ok";
+	public List<Categoria> findAll() {
+		 Categoria c1 = new Categoria(1, "test");
+		 Categoria c2 = new Categoria(2, "test2");
+		 List<Categoria> list = new ArrayList<>();
+		 list.addAll(Arrays.asList(c1,c2));
+		 return list;
 	}
 
 }
