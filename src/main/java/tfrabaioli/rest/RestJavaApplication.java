ad29ec00b61fe1fe48d263dbf7e49ffe2805f5bb
thiagorabaioli <tfrabaioli@outlook.com>
2023-05-31 13:30:20 +0100

    CommandLineRunner instantiation

--- src/main/java/tfrabaioli/rest/RestJavaApplication.java
+++ src/main/java/tfrabaioli/rest/RestJavaApplication.java
@@ -1,13 +1,32 @@
 package tfrabaioli.rest;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RestJavaApplication {
+import tfrabaioli.rest.domain.Categoria;
+import tfrabaioli.rest.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class RestJavaApplication  implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RestJavaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepo.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
