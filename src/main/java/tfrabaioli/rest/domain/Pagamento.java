5af37ca129c90c005efde370366395b510878c18
thiagorabaioli <tfrabaioli@outlook.com>
2023-06-03 15:35:47 +0100

    association Pagamento and Pedido with same Id

--- src/main/java/tfrabaioli/rest/domain/Pagamento.java
+++ src/main/java/tfrabaioli/rest/domain/Pagamento.java
@@ -3,16 +3,28 @@ package tfrabaioli.rest.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
 import tfrabaioli.rest.domain.enums.EstadoPagamento;
 
+
+@Entity
 public abstract class Pagamento implements Serializable {
 	
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private Integer id;
 	private EstadoPagamento estado;
 	
+	@OneToOne
+	@JoinColumn(name="pedido_id")
+	@MapsId
 	private Pedido pedido;
 	
 	public Pagamento() {}
