7f6c2bbf66d3b560bb44b1c5295f5c5c05c89871
thiagorabaioli <tfrabaioli@outlook.com>
2023-06-03 15:32:24 +0100

    Pedido, PagamentoComBoleto, PagamentoComCartao classes

--- src/main/java/tfrabaioli/rest/domain/Cliente.java
+++ src/main/java/tfrabaioli/rest/domain/Cliente.java
@@ -32,6 +32,8 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
@@ -53,6 +55,14 @@ public class Cliente implements Serializable {
 	}
 	
 	
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	public Set<String> getTelefones() {
 		return telefones;
